98e7673d8403687c496c55d53e955fdd121f7837
Dominik <dominikant99@gmail.com>
2023-08-13 13:05:58 +0200

    Create resetBoard method to gameBoard module

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const gameBoard = (() => {
-    const boardContainer = document.querySelector(".board-container");
+  const boardContainer = document.querySelector(".board-container");
 
-    const board = Array(9).fill("");
+  const board = Array(9).fill("");
 
-    const displayBoard = () => {
-        board.forEach((cell, index) => {
-            const divCell = document.createElement("div");
-            divCell.textContent = "X";
-            divCell.classList.add("cell");
-            divCell.dataset.cell = `${index}`;
-            boardContainer.append(divCell);
-        });
-    };
+  const displayBoard = () => {
+    board.forEach((cell, index) => {
+      const divCell = document.createElement("div");
+      divCell.textContent = "X";
+      divCell.classList.add("cell");
+      divCell.dataset.cell = `${index}`;
+      boardContainer.append(divCell);
+    });
+  };
 
-    displayBoard();
+  const resetBoard = () => {
+    const cellsNodeList = document.querySelectorAll(".cell");
 
-    return {
-        board,
-        displayBoard,
-    };
+    cellsNodeList.forEach((cell) => {
+      if (cell.classList.contains("cell")) {
+        cell.textContent = "";
+      }
+    });
+  };
+
+  displayBoard();
+
+  return {
+    board,
+    displayBoard,
+    resetBoard,
+  };
 })();
+
+const displayController = () => {
+  let mark;
+};
+
+const playerFactory = (name, mark) => {
+  return { name, mark };
+};
